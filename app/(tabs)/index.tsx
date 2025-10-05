b032fd720082055f8a732aa0920330d8d01f693e
Toroidal <28606655+Toroidal@users.noreply.github.com>
2025-10-05 08:26:05 +0100

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello world!</ThemedText>
+        <ThemedText type="title">Test!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
